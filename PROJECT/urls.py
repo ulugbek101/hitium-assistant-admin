18838cb51171c828f782b7bcdac5264055175b70
Ulugbek Umaraliyev <thedevu101@gmail.com>
2026-01-07 09:28:02 +0000

    Make admin urls as default

--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
     path("api/v1/", include("api.urls")),
-    path("admin/", admin.site.urls)
+    path("", admin.site.urls)
 ]
 
 urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
